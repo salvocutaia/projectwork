184d66f26ce2a7939c6d52596ba9ab14c6b6a3e7
Luigi <Luigi@luigigraci-PC>
2018-02-15 15:32:25 +0100

    imposta metodo get user in GET

--- projectwork-web/src/main/java/it/eng/unipa/projectwork/web/UserManagementRestEndpoint.java
+++ projectwork-web/src/main/java/it/eng/unipa/projectwork/web/UserManagementRestEndpoint.java
@@ -3,6 +3,7 @@ package it.eng.unipa.projectwork.web;
 import javax.annotation.security.RolesAllowed;
 import javax.ejb.Stateless;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -25,7 +26,7 @@ public class UserManagementRestEndpoint {
 	
 
 	
-	@POST
+	@GET
 	@Path("/get")
 	@RolesAllowed(value="USER")
    public UserDTO get(@Context SecurityContext sc){
